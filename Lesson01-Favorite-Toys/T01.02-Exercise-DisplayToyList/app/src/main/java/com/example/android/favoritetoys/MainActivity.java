e83e843e01f844dcc047dbd02c771c2bad34a546
Vicuko <victorc.fernandez@gmail.com>
2018-05-02 23:42:06 -0500

    Declare new variables

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -17,10 +17,14 @@ package com.example.android.favoritetoys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Declare a TextView variable called mToysListTextView
+    TextView mToysListTextView;
+    // TODO (1) Declare a TextView variable called mToysListTextView - Done
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
