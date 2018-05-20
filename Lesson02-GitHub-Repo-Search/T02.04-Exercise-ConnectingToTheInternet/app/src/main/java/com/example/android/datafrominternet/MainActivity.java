14fe198db12839fa1ea00e0fe77d633af58dcd03
Vicuko <victorc.fernandez@gmail.com>
2018-05-20 12:20:30 -0400

    Rebuild project and implement url call

--- Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,8 +56,15 @@ public class MainActivity extends AppCompatActivity {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-        // TODO (2) Call getResponseFromHttpUrl and display the results in mSearchResultsTextView
-        // TODO (3) Surround the call to getResponseFromHttpUrl with a try / catch block to catch an IOException
+        // TODO (2) Call getResponseFromHttpUrl and display the results in mSearchResultsTextView - Done
+        // TODO (3) Surround the call to getResponseFromHttpUrl with a try / catch block to catch an IOException - Done
+        String githubSearchResult = null;
+        try {
+            githubSearchResult = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
+            mSearchResultsTextView.setText(githubSearchResult);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
