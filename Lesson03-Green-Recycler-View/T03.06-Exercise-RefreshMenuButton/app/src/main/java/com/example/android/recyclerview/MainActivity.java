b7c05291f5786c31951e81c7ea65ceb6531c4cd4
Vicuko <victorc.fernandez@gmail.com>
2018-07-11 12:41:18 -0400

    Define action when clicking on menu item overriding onOptionsItemSelected to be creating a new adapter and reassigning it to the recyclerView

--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -20,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -87,9 +88,20 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // TODO (10) Override onOptionsItemSelected - Done
+    // TODO (11) Within this method, get the ID from the MenuItem - Done
+    // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true - Done
+    // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected - Done
 
-    // TODO (10) Override onOptionsItemSelected
-    // TODO (11) Within this method, get the ID from the MenuItem
-    // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
-    // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh){
+            GreenAdapter newAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+            mNumbersList.setAdapter(newAdapter);
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
