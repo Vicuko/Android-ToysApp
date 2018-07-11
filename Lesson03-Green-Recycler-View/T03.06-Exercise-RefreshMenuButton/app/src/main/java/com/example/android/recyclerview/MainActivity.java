5a8fe9978fc532848c263e582f356333b39a1abc
Vicuko <victorc.fernandez@gmail.com>
2018-07-11 12:34:51 -0400

    Inflate menu using getMenuInflater().inflate() to the menu

--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -76,9 +77,16 @@ public class MainActivity extends AppCompatActivity {
     // TODO (6) Set app:showAsAction to ifRoom to display the menu item in the ActionBar if there is room - Done
 
 
-    // TODO (7) Override onCreateOptionsMenu
-    // TODO (8) Use getMenuInflater().inflate to inflate the menu
-    // TODO (9) Return true to display this menu
+    // TODO (7) Override onCreateOptionsMenu - Done
+    // TODO (8) Use getMenuInflater().inflate to inflate the menu - Done
+    // TODO (9) Return true to display this menu - Done
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
 
     // TODO (10) Override onOptionsItemSelected
     // TODO (11) Within this method, get the ID from the MenuItem
