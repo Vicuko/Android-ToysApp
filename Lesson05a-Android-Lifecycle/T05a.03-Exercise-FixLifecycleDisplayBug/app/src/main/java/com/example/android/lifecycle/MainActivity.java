60fe9c9c11a02a6ae8ead1edeba62dff8ddb0c8c
Vicuko <victorc.fernandez@gmail.com>
2018-08-08 14:43:53 -0400

    Include onDestroy and onStop messages into array to be displayed onCreate (after rotating screen for example)

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks - Done
+
+    private static final ArrayList<String> mLifecycleCallbacks = new ArrayList<>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,9 +75,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
-
+        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay - Done
+        for (int i = mLifecycleCallbacks.size(); i>0; i--){
+            String currentTextView = mLifecycleDisplay.getText().toString();
+            String newTextViewContent = currentTextView + mLifecycleCallbacks.get(i-1) + "\n";
+            mLifecycleDisplay.setText(newTextViewContent);
+        }
         // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,8 +142,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks - Done
+        mLifecycleCallbacks.add(ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -160,8 +169,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks - Done
+        mLifecycleCallbacks.add(ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
