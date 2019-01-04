fd62ed5767fddfa42907ad4618acfd19596db37b
Vicuko <victorc.fernandez@gmail.com>
2019-01-04 19:19:30 +0100

    Create a TaskDbHelper object in TaskContentProvider and initialize it in the onCreate method

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -18,14 +18,15 @@ package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
+// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods - Done
 public class TaskContentProvider extends ContentProvider {
 
-
+    public TaskDbHelper mTaskDbHelper;
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
     In this case, you’re working with a SQLite database, so you’ll need to
@@ -33,10 +34,11 @@ public class TaskContentProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
+        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup - Done
         // [Hint] Declare the DbHelper as a global variable
-
-        return false;
+        Context context = getContext();
+        mTaskDbHelper = new TaskDbHelper(context);
+        return true;
     }
 
 
