75016faf2dafd6e80e6dec0fd30b8c3b47d56025
Vicuko <victorc.fernandez@gmail.com>
2019-01-08 14:56:58 +0100

    Update code to run query method from Room in an independent thread

--- Lesson09b-ToDo-List-AAC/T09b.04-Exercise-Executors/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.04-Exercise-Executors/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -115,13 +115,19 @@ public class AddTaskActivity extends AppCompatActivity {
         int priority = getPriorityFromViews();
         Date date = new Date();
 
-        // TODO (4) Make taskEntry final so it is visible inside the run method
-        TaskEntry taskEntry = new TaskEntry(description, priority, date);
-        // TODO (2) Get the diskIO Executor from the instance of AppExecutors and
+        // TODO (4) Make taskEntry final so it is visible inside the run method - Done
+        final TaskEntry taskEntry = new TaskEntry(description, priority, date);
+        // TODO (2) Get the diskIO Executor from the instance of AppExecutors and - Done
         // call the diskIO execute method with a new Runnable and implement its run method
-        // TODO (3) Move the remaining logic inside the run method
-        mDb.taskDao().insertTask(taskEntry);
-        finish();
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                // TODO (3) Move the remaining logic inside the run method - Done
+                mDb.taskDao().insertTask(taskEntry);
+                finish();
+            }
+        });
+
     }
 
     /**
