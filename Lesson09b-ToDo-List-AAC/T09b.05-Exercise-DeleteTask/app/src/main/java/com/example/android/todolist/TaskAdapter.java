ea257277d74ba2d1acb81ef52c5c36f1f28c900f
Vicuko <victorc.fernandez@gmail.com>
2019-01-18 19:38:18 +0100

    Create getTasks method in Adapter to return all Task Entries

--- Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/TaskAdapter.java
+++ Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/TaskAdapter.java
@@ -135,8 +135,10 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         return mTaskEntries.size();
     }
 
-    // TODO (2) Add a getTasks method that returns mTaskEntries
-
+    // TODO (2) Add a getTasks method that returns mTaskEntries - Done
+    public List<TaskEntry> getTasks (){
+        return mTaskEntries;
+    }
     /**
      * When data changes, this method updates the list of taskEntries
      * and notifies the adapter to use the new values on it
