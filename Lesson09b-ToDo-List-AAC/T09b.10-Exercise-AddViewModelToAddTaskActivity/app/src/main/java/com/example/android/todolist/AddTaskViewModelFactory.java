0413eea92241a9bdbbc81a956b9e03895d1cac37
Vicuko <victorc.fernandez@gmail.com>
2019-01-20 21:25:23 +0100

    Add two member variables. One for the database and one for the taskId

--- Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
+++ Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
@@ -2,10 +2,13 @@ package com.example.android.todolist;
 
 import android.arch.lifecycle.ViewModelProvider;
 
+import com.example.android.todolist.database.AppDatabase;
+
 // TODO (1) Make this class extend ViewModel ViewModelProvider.NewInstanceFactory - Done
 public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactory {
-
-    // TODO (2) Add two member variables. One for the database and one for the taskId
+    // TODO (2) Add two member variables. One for the database and one for the taskId - Done
+    private final AppDatabase mDb;
+    private final int mTaskId;
 
     // TODO (3) Initialize the member variables in the constructor with the parameters received
 
