f3b820bb4e2dd22c34fd071bf4621d5e2949d4c9
Vicuko <victorc.fernandez@gmail.com>
2019-01-20 20:44:02 +0100

    Add log for constructor

--- Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.example.android.todolist.database.AppDatabase;
 import com.example.android.todolist.database.TaskEntry;
@@ -23,6 +24,7 @@ public class MainViewModel extends AndroidViewModel {
         super(application);
         // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable - Done
         AppDatabase appDatabase = AppDatabase.getInstance(this.getApplication());
+        Log.d(TAG, "Actively retrieving the tasks from the Database");
         tasks = appDatabase.taskDao().loadAllTasks();
     }
 
