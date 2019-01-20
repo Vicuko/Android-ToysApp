a2ff4ef02795b470ca2c055426d99c1aaaff8c6f
Vicuko <victorc.fernandez@gmail.com>
2019-01-20 20:47:08 +0100

    Observe the LiveData object in the ViewModel

--- Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.example.android.todolist;
 
-import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
@@ -119,8 +118,8 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         // TODO (5) Remove the logging and the call to loadAllTasks, this is done in the ViewModel now - Done
         // TODO (6) Declare a ViewModel variable and initialize it by calling ViewModelProviders.of - Done
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        // TODO (7) Observe the LiveData object in the ViewModel
-        tasks.observe(this, new Observer<List<TaskEntry>>() {
+        // TODO (7) Observe the LiveData object in the ViewModel - Done
+        viewModel.getTasks().observe(this, new Observer<List<TaskEntry>>() {
             @Override
             public void onChanged(@Nullable List<TaskEntry> taskEntries) {
                 Log.d(TAG, "Receiving database update from LiveData");
