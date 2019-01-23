493c3d1e7ab0d6857668a5a6a03f5e7725c5731a
Vicuko <victorc.fernandez@gmail.com>
2019-01-23 21:34:52 +0100

    If the water count was incremented, clear any notifications

--- Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -38,6 +38,7 @@ public class ReminderTasks {
 
     private static void incrementWaterCount(Context context) {
         PreferenceUtilities.incrementWaterCount(context);
-        //      TODO (4) If the water count was incremented, clear any notifications
+        //      TODO (4) If the water count was incremented, clear any notifications - Done
+        NotificationUtils.clearAllNotifications(context);
     }
 }
