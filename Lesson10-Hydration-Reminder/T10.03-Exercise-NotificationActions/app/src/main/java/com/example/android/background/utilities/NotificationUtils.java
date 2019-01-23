d8b5c59eac396c8b52cb673e55450e675d2ceafb
Vicuko <victorc.fernandez@gmail.com>
2019-01-23 21:46:15 +0100

    Set the action of the intent to designate you want to increment the water count

--- Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -115,8 +115,9 @@ public class NotificationUtils {
     //  TODO (11) Add a static method called drinkWaterAction - Done
     private static NotificationCompat.Action drinkWaterAction(Context context) {
         //      TODO (12) Create an Intent to launch WaterReminderIntentService - Done
-        Intent drinkWaterIntent = new Intent(context, WaterReminderIntentService.class);
-        //      TODO (13) Set the action of the intent to designate you want to increment the water count
+        Intent incrementWaterIntent = new Intent(context, WaterReminderIntentService.class);
+        //      TODO (13) Set the action of the intent to designate you want to increment the water count - Done
+        incrementWaterIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
         //      TODO (14) Create a PendingIntent from the intent to launch WaterReminderIntentService
         //      TODO (15) Create an Action for the user to tell us they've had a glass of water
         //      TODO (16) Return the action
