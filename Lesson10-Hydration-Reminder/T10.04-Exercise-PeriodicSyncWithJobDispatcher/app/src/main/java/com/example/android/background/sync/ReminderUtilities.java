a82c7a36f8d2be61129bb627ba69120c0a487e4c
Vicuko <victorc.fernandez@gmail.com>
2019-01-24 14:16:30 +0100

    Create constants and variable to be used afterwards in order to schedule the job

--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
@@ -16,6 +16,8 @@
 package com.example.android.background.sync;
 
 
+import java.util.concurrent.TimeUnit;
+
 public class ReminderUtilities {
     // TODO (15) Create three constants and one variable:
     //  - REMINDER_INTERVAL_SECONDS should be an integer constant storing the number of seconds in 15 minutes
@@ -23,11 +25,17 @@ public class ReminderUtilities {
     //  - REMINDER_JOB_TAG should be a String constant, storing something like "hydration_reminder_tag"
     //  - sInitialized should be a private static boolean variable which will store whether the job
     //    has been activated or not
+    private static final int REMINDER_INTERVAL_MINUTES = 15;
+    private static final int REMINDER_INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES));
+    private static final int SYNC_FLEXTIME_SECONDS = REMINDER_INTERVAL_SECONDS;
+    private static final String REMINDER_JOB_TAG = "hydration_reminder_tag";
+    private static boolean sInitialized;
 
     // TODO (16) Create a synchronized, public static method called scheduleChargingReminder that takes
     // in a context. This method will use FirebaseJobDispatcher to schedule a job that repeats roughly
     // every REMINDER_INTERVAL_SECONDS when the phone is charging. It will trigger WaterReminderFirebaseJobService
     // Checkout https://github.com/firebase/firebase-jobdispatcher-android for an example
+
         // TODO (17) If the job has already been initialized, return
         // TODO (18) Create a new GooglePlayDriver
         // TODO (19) Create a new FirebaseJobDispatcher with the driver
